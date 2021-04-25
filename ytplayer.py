562fc72b92d65c562f2fa41b5b293ec62e030df1
szyjah <44848849+szyjah@users.noreply.github.com>
2021-04-25 13:41:13 +0200

    quick fix
    We don't like exceptions in a discord chat lel

--- ytplayer.py
+++ ytplayer.py
@@ -27,41 +27,43 @@ class YTPlayer(commands.Cog):
     @commands.command(name='play')
     async def _jutub(self,ctx: commands.Context, *args):     
         """Play audio form youtube. Uses first result of a youtube search."""
-        
-        channel = ctx.message.author.voice.channel
-        if(len(self.bot.voice_clients) == 0 and channel != None):
-            video_id = None
-            if len(args) == 1:
-                web_link_regex_long = r'(https://)?(www\.)?youtube\.com/watch\?.*v=(\S{11}).*'
-                web_link_regex_short = r'(https://)?youtu\.be/(\S{11}).*'
-                match_long = re.match(web_link_regex_long, args[0])
-                match_short = re.match(web_link_regex_short, args[0])
-                if match_long != None:
-                    video_id = match_long[3]
-                elif match_short != None:
-                    video_id = match_short[2]
+        if not ctx.message.author.voice:
+            await ctx.send("You need to be in a vc to use this command")
+        else:
+            channel = ctx.message.author.voice.channel
+            if(len(self.bot.voice_clients) == 0 and channel != None):
+                video_id = None
+                if len(args) == 1:
+                    web_link_regex_long = r'(https://)?(www\.)?youtube\.com/watch\?.*v=(\S{11}).*'
+                    web_link_regex_short = r'(https://)?youtu\.be/(\S{11}).*'
+                    match_long = re.match(web_link_regex_long, args[0])
+                    match_short = re.match(web_link_regex_short, args[0])
+                    if match_long != None:
+                        video_id = match_long[3]
+                    elif match_short != None:
+                        video_id = match_short[2]
 
-            if video_id == None:
-                search = ""
-                for x in args:
-                    search = search + str(x) + "+" 
-                html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search)
-                video_id = re.findall(r"watch\?v=(\S{11})", html.read().decode())[0]
-            
-            song = pafy.new(video_id)
-            url = song.getbestaudio().url
-            
-            self.voice = await channel.connect()
+                if video_id == None:
+                    search = ""
+                    for x in args:
+                        search = search + str(x) + "+" 
+                    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search)
+                    video_id = re.findall(r"watch\?v=(\S{11})", html.read().decode())[0]
 
-            e = discord.Embed(title='Now playing', description=f"Title: *** {song.title} *** \nTime: {song.duration}", url=song.watchv_url)
-            e.set_thumbnail(url=song.bigthumb)
-            await ctx.send(embed=e)
-                         
-            source = discord.FFmpegPCMAudio(url, **FFMPEG_OPTIONS)
-            self.voice.play(source)
-            while(self.voice.is_playing() or self.voice.is_paused()):
-                await asyncio.sleep(1)
-            await self.voice.disconnect()                    
+                song = pafy.new(video_id)
+                url = song.getbestaudio().url
+
+                self.voice = await channel.connect()
+
+                e = discord.Embed(title='Now playing', description=f"Title: *** {song.title} *** \nTime: {song.duration}", url=song.watchv_url)
+                e.set_thumbnail(url=song.bigthumb)
+                await ctx.send(embed=e)
+
+                source = discord.FFmpegPCMAudio(url, **FFMPEG_OPTIONS)
+                self.voice.play(source)
+                while(self.voice.is_playing() or self.voice.is_paused()):
+                    await asyncio.sleep(1)
+                await self.voice.disconnect()                    
 
     @commands.command(name='pause')
     async def _pause(self,ctx: commands.Context):

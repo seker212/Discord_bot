0df1a831f4332a3d382d70d19eace13c22bf90e0
seker212 <seker212@gmail.com>
2023-04-01 00:10:57 +0200

    Added record class for command options

---
+++ DiscordBot.Commands.Core/CommandOption.cs
@@ -0,0 +1,21 @@
+﻿using Discord;
+using DiscordBot.Commands.Core.CommandAttributes;
+
+namespace DiscordBot.Commands.Core
+{
+    public record CommandOption
+    {
+        public CommandOption(string name, string description, ApplicationCommandOptionType type)
+        {
+            Name = name;
+            Description = description;
+            Type = type;
+        }
+
+        public CommandOption(OptionAttribute optionAttribute) : this(optionAttribute.Name, optionAttribute.Description, optionAttribute.Type) { }
+
+        public string Name { get; }
+        public string Description { get; }
+        public ApplicationCommandOptionType Type { get; }
+    }
+}

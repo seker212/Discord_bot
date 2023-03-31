aed16c60929b093cc10a6beb8b8b2a39116110c5
seker212 <seker212@gmail.com>
2023-03-31 22:56:57 +0200

    Created MessageReceivedHandlerProvider

---
+++ DiscordBot.Core/Providers/MessageReceivedHandlerProvider.cs
@@ -0,0 +1,23 @@
+﻿using Discord.WebSocket;
+using DiscordBot.Core.MessageReceivedHandlers;
+
+namespace DiscordBot.Core.Providers
+{
+    public interface IMessageReceivedHandlerProvider
+    {
+        Task OnMessageReceived(SocketMessage socketMessage);
+    }
+
+    public class MessageReceivedHandlerProvider : IMessageReceivedHandlerProvider
+    {
+        private readonly IEnumerable<IMessageReceivedHandler> _handlers;
+
+        public MessageReceivedHandlerProvider(IEnumerable<IMessageReceivedHandler> handlers)
+        {
+            _handlers = handlers;
+        }
+
+        public Task OnMessageReceived(SocketMessage socketMessage)
+            => Task.Run(() => Task.WaitAll(_handlers.Select(x => x.Execute(socketMessage)).ToArray()));
+    }
+}

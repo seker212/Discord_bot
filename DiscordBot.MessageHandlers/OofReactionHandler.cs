93a38fb84fcfd584b8186c98f4d7f3c0c8f3e1eb
seker212 <seker212@gmail.com>
2023-04-03 17:22:16 +0200

    Added log entry to oof reaction handler

--- DiscordBot.MessageHandlers/OofReactionHandler.cs
+++ DiscordBot.MessageHandlers/OofReactionHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Discord.WebSocket;
 using DiscordBot.Core.Interfaces;
+using Microsoft.Extensions.Logging;
 using System.Text.RegularExpressions;
 
 namespace DiscordBot.MessageHandlers
@@ -10,19 +11,26 @@ namespace DiscordBot.MessageHandlers
     public class OofReactionHandler : IMessageReceivedHandler
     {
         private const string OOF_PATTERN = @"(\s|^)[oO]{2,}[fF](\s|$)";
+        private const string EMOTE_NAME = "oof";
+        private readonly ILogger<OofReactionHandler> _logger;
+
+        public OofReactionHandler(ILogger<OofReactionHandler> logger)
+        {
+            _logger = logger;
+        }
 
         private bool IsApplicable(SocketMessage socketMessage)
             => socketMessage.Channel is SocketTextChannel socketChannel &&
-            socketChannel.Guild.Emotes.Any(x => x.Name.ToLower() == "oof") &&
+            socketChannel.Guild.Emotes.Any(x => x.Name.ToLower() == EMOTE_NAME) &&
             Regex.IsMatch(socketMessage.Content, OOF_PATTERN);
 
-        private Task ApplyReactionAsync(SocketMessage socketMessage)
-            => Task.Run(async () =>
-            {
-                var socketChannel = (socketMessage.Channel as SocketTextChannel)!;
-                var emote = socketChannel.Guild.Emotes.SingleOrDefault(x => x.Name.ToLower() == "oof");
-                await socketMessage.AddReactionAsync(emote);
-            });
+        private async Task ApplyReactionAsync(SocketMessage socketMessage)
+        {
+            _logger.LogDebug("Applying oof reaction to message");
+            var socketChannel = (socketMessage.Channel as SocketTextChannel)!;
+            var emote = socketChannel.Guild.Emotes.SingleOrDefault(x => x.Name.ToLower() == EMOTE_NAME);
+            await socketMessage.AddReactionAsync(emote);
+        }
 
         public Task Execute(SocketMessage socketMessage)
             => Task.Run(async () =>

2256b9b3a131b99e1c8d846c0412e50894940435
seker212 <seker212@gmail.com>
2023-04-05 13:48:30 +0200

    Added IsRequired to option attribute

--- DiscordBot.Commands.Core/CommandAttributes/OptionAttribute.cs
+++ DiscordBot.Commands.Core/CommandAttributes/OptionAttribute.cs
@@ -5,15 +5,17 @@ namespace DiscordBot.Commands.Core.CommandAttributes
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     public class OptionAttribute : Attribute
     {
-        public OptionAttribute(string name, string description, ApplicationCommandOptionType type)
+        public OptionAttribute(string name, string description, ApplicationCommandOptionType type, bool isRequired = true)
         {
             Name = name;
             Description = description;
             Type = type;
+            IsRequired = isRequired;
         }
 
         public string Name { get; }
         public string Description { get; }
         public ApplicationCommandOptionType Type { get; }
+        public bool IsRequired { get; }
     }
 }

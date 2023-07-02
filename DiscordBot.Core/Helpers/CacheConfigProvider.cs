6e2dc6cf2f15d08bbd360ba3c4153c95586c7589
seker212 <seker212@gmail.com>
2023-07-02 15:36:53 +0200

    Created caching config provider

---
+++ DiscordBot.Core/Helpers/CacheConfigProvider.cs
@@ -0,0 +1,40 @@
+﻿using DiscordBot.Core.Interfaces;
+
+namespace DiscordBot.Core.Helpers
+{
+    public class CacheConfigProvider : IConfigProvider
+    {
+        private readonly Dictionary<ulong, Dictionary<string, string?>> _parametersCache;
+
+        public CacheConfigProvider(Dictionary<ulong, Dictionary<string, string?>> parametersCache)
+        {
+            _parametersCache = parametersCache;
+        }
+
+        public string? GetParameter(ulong? guildId, string parameterName)
+        {
+            var convertedGuildId = guildId ?? 0;
+            if (!_parametersCache.ContainsKey(convertedGuildId))
+                return null;
+            var guildParams = _parametersCache[convertedGuildId];
+            if (!guildParams.ContainsKey(parameterName))
+                return null;
+            return guildParams[parameterName];
+        }
+
+        public void SetParameter(ulong? guildId, string parameterName, string? parameterValue)
+        {
+            var convertedGuildId = guildId ?? 0;
+            if (!_parametersCache.ContainsKey(convertedGuildId))
+                _parametersCache.Add(convertedGuildId, new() { { parameterName, parameterValue } });
+            else
+            {
+                var guildParams = _parametersCache[convertedGuildId];
+                if (!guildParams.ContainsKey(parameterName))
+                    guildParams.Add(parameterName, parameterValue);
+                else
+                    guildParams[parameterName] = parameterValue;
+            }
+        }
+    }
+}

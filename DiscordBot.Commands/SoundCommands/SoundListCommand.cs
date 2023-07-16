4aeb4539313e74c03d075047b8ae6c1e4003114e
Niewidzialny84 <niewidzialny84@vp.pl>
2023-07-16 20:41:50 +0200

    Added listing sounds, uploading/removing sounds. Moved everything to a separate folder.

---
+++ DiscordBot.Commands/SoundCommands/SoundListCommand.cs
@@ -0,0 +1,79 @@
+﻿using Discord;
+using Discord.WebSocket;
+using DiscordBot.Commands.Core;
+using DiscordBot.Commands.Core.CommandAttributes;
+using Microsoft.Extensions.Logging;
+
+namespace DiscordBot.Commands.SoundCommands
+{
+    [Name("soundlist")]
+    [Description("Prints the list of available sounds")]
+    [Option("filter", "For getting only sound starting from given value", CommandOptionType.String, false)]
+    public class SoundListCommand : AbstractSoundCommand
+    {
+        private readonly ILogger<SoundListCommand> _logger;
+        private readonly List<char> alphabet = new List<char>{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', 'Z'};
+
+        public SoundListCommand(ILogger<SoundListCommand> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task ExecuteAsync(SocketSlashCommand command)
+        {
+            command.DeferAsync().Wait();
+
+            try
+            {
+                var filter = command.GetOptionValue("filter") as string;
+
+                if (filter != null) 
+                {
+                    await command.ModifyOriginalResponseAsync(m => m.Embed = GetSoundListEmbed(filter));
+                }
+                else
+                {
+                    await command.ModifyOriginalResponseAsync(m => m.Embed = GetSoundListEmbed());
+                }
+            }
+            catch (Exception ex) 
+            {
+                _logger.LogError(ex, "Printing sound list threw an exception");
+                await command.ModifyOriginalResponseAsync(m => m.Content = "Error occured when handling command");
+            }
+        }
+
+        private Embed GetSoundListEmbed(string filter = "")
+        {
+            var soundNames = GetSoundNames();
+            var embed = new EmbedBuilder()
+            {
+                Title = "Sound list",
+                Color = Color.Teal,
+            };
+
+            if (filter != string.Empty)
+            {
+                soundNames = soundNames.Where(x => x.StartsWith(filter));
+
+                embed.WithDescription("List of all sounds with applied filter: " + filter);
+            }
+            else
+            {
+                embed.WithDescription("List of all sounds");
+            }
+
+            foreach (var letter in alphabet)
+            {
+                string text = string.Join("\n", soundNames.Where(x => x.ToUpper().First().Equals(letter)));
+
+                if (text != string.Empty)
+                {
+                    embed.AddField(letter.ToString(), text);
+                }
+            }
+
+            return embed.Build();
+        }
+    }
+}
---
+++ DiscordBot.Commands/SoundCommands/SoundUploadCommand.cs
@@ -0,0 +1,65 @@
+﻿using Discord.WebSocket;
+using DiscordBot.Commands.Core.CommandAttributes;
+using DiscordBot.Commands.Core;
+using Microsoft.Extensions.Logging;
+using Discord;
+
+namespace DiscordBot.Commands.SoundCommands
+{
+    [Name("soundupload")]
+    [Description("For uploading new sounds for bot to play")]
+    [Option("file", "File in mp3 fromat", CommandOptionType.Attachment)]
+    public class SoundUploadCommand : AbstractSoundCommand
+    {
+        private readonly ILogger<SoundUploadCommand> _logger;
+
+        public SoundUploadCommand(ILogger<SoundUploadCommand> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task ExecuteAsync(SocketSlashCommand command)
+        {
+            command.DeferAsync().Wait();
+
+            try
+            {
+                var attachment = command.GetRequiredOptionValue("file") as Attachment;
+                
+                if (attachment.Filename.EndsWith(".mp3")) 
+                {
+                    using (var client = new HttpClient())
+                    {
+                        var response = await client.GetAsync(attachment.Url);
+                        var fileMode = FileMode.CreateNew;
+                        var message = "File " + attachment.Filename + " uploaded";
+
+                        if (GetSoundNames().Contains(attachment.Filename))
+                        {
+                            fileMode = FileMode.Truncate;
+                            message = "File " + attachment.Filename + " was replaced";
+                        }
+
+                        using (var file = new FileStream(AudioDirectoryPath + "/" + attachment.Filename, fileMode))
+                        {
+                            await response.Content.CopyToAsync(file);
+                            await command.ModifyOriginalResponseAsync(m => {
+                                m.Content = message;
+                                m.Attachments = new List<FileAttachment> { new FileAttachment(file, attachment.Filename) };
+                                });
+                        }
+                    }
+                }
+                else
+                {
+                    await command.ModifyOriginalResponseAsync(m => m.Content = "Invalid file extension, should be .mp3");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Uploading file threw an exception");
+                await command.ModifyOriginalResponseAsync(m => m.Content = "Error occured when handling command");
+            }
+        }
+    }
+}

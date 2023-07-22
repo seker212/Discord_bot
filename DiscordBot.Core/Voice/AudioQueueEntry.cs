1d7c8dd42fc6a9248e4f375da6e5865016e50c38
seker212 <seker212@gmail.com>
2023-07-22 12:13:40 +0200

    Added queue entry and player

---
+++ DiscordBot.Core/Voice/AudioQueueEntry.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+
+namespace DiscordBot.Core.Voice
+{
+    public sealed class AudioQueueEntry
+    {
+        public AudioQueueEntry(IVoiceChannel channel, Lazy<Stream> audioStream, Action? onStart, Action? onFinish, IDictionary<string, object?>? logProperties)
+        {
+            Channel = channel;
+            AudioStream = audioStream;
+            OnStart = onStart;
+            OnFinish = onFinish;
+            LogProperties = logProperties ?? new Dictionary<string, object?>();
+        }
+
+        public IVoiceChannel Channel { get; }
+        public Lazy<Stream> AudioStream { get; }
+        public Action? OnStart { get; }
+        public Action? OnFinish { get; }
+        public IDictionary<string, object?> LogProperties { get; }
+    }
+}
---
+++ DiscordBot.Core/Voice/AudioQueueManager.cs
@@ -0,0 +1,70 @@
+﻿using Discord.Audio;
+using Microsoft.Extensions.Logging;
+
+namespace DiscordBot.Core.Voice
+{
+    public class AudioQueueManager
+    {
+        private readonly IDictionary<ulong, Queue<AudioQueueEntry>> _guildsQueues;
+        private readonly IAudioClientManager _audioClientManager;
+        private readonly IDictionary<ulong, Task> _playingTaskCache;
+        private readonly ILogger<AudioQueueManager> _logger;
+
+
+        public int Add(ulong guildId, AudioQueueEntry audioQueueEntry)
+        {
+            if (_guildsQueues.ContainsKey(guildId))
+                _guildsQueues[guildId].Enqueue(audioQueueEntry);
+            else
+                _guildsQueues.Add(guildId, new Queue<AudioQueueEntry>(new[] { audioQueueEntry }));
+            
+            if (_playingTaskCache.ContainsKey(guildId))
+            {
+                if (!_playingTaskCache[guildId].IsCompleted)
+                    _playingTaskCache[guildId] = PlayQueueAsync(guildId);
+            }
+            else
+                _playingTaskCache.Add(guildId, PlayQueueAsync(guildId));
+
+            return _guildsQueues[guildId].Count;
+        }
+
+        public void Skip(ulong guildId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop(ulong guildId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task PlayQueueAsync(ulong guildId)
+        {
+            using (_logger.BeginScope(new Dictionary<string, object?>() { { "GuildId", guildId } }))
+                while (_guildsQueues[guildId].Any())
+                {
+                    var currentEntry = _guildsQueues[guildId].Dequeue();
+                    IAudioClient? audioClient = null;
+
+                    using (_logger.BeginScope(currentEntry.LogProperties))
+                    {
+                        if (_audioClientManager.HasActiveAudioClient(guildId))
+                            if (_audioClientManager.GetGuildActiveVoiceChannel(guildId) != currentEntry.Channel) //TODO: Check if IVoiceChannel is equalable based on id
+                                await _audioClientManager.LeaveChannelAsync(currentEntry.Channel);
+                            else
+                                audioClient = _audioClientManager.GetGuildAudioClient(guildId);
+                        if (audioClient is null)
+                            audioClient = await _audioClientManager.JoinChannelAsync(currentEntry.Channel);
+
+                        var player = _audioClientManager.GetAudioPlayer(audioClient);
+                        if (currentEntry.OnStart is not null)
+                            currentEntry.OnStart.Invoke();
+                        await player.PlayAsync(currentEntry.AudioStream.Value);
+                        if (currentEntry.OnFinish is not null)
+                            currentEntry.OnFinish.Invoke();
+                    }
+                }
+        }
+    }
+}

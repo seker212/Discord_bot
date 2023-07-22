e3989940a0090dbc1cbe9eaa548149f8db830e89
seker212 <seker212@gmail.com>
2023-07-22 19:21:19 +0200

    Updated sound command

--- DiscordBot.Commands/SoundCommands/SoundCommand.cs
+++ DiscordBot.Commands/SoundCommands/SoundCommand.cs
@@ -14,74 +14,78 @@ namespace DiscordBot.Commands.SoundCommands
     [Option("channel", "voice channel", CommandOptionType.GuildVoiceChannel)]
     public class SoundCommand : AbstractSoundCommand
     {
+        private readonly IAudioQueueManager _audioQueueManager;
         private readonly IAudioClientManager _audioClientManager;
         private readonly ILogger<SoundCommand> _logger;
 
-        public SoundCommand(IAudioClientManager audioClientManager, ILogger<SoundCommand> logger)
+        public SoundCommand(IAudioClientManager audioClientManager, ILogger<SoundCommand> logger, IAudioQueueManager audioQueueManager)
         {
             _audioClientManager = audioClientManager;
             _logger = logger;
+            _audioQueueManager = audioQueueManager;
         }
 
-        public override Task ExecuteAsync(SocketSlashCommand command)
+        public override async Task ExecuteAsync(SocketSlashCommand command)
         {
-            command.DeferAsync().Wait();
-            Task.Run(async () =>
+            await command.DeferAsync();
+            
+            try
             {
-                try
+                var soundName = command.GetRequiredOptionValue<string>("soundname");
+                var audioFile = new FileInfo(Path.Combine(AudioDirectoryPath, $"{soundName}.mp3"));
+                _logger.LogDebug("Searching audio file in {dir} directory", audioFile.Directory?.FullName);
+                if (!audioFile.Exists)
                 {
-                    var soundName = command.Data.Options.Single(x => x.Name == "soundname").Value as string;
-                    var audioFile = new FileInfo(Path.Combine(AudioDirectoryPath, $"{soundName}.mp3"));
-                    _logger.LogDebug("Searching audio file in {dir} directory", audioFile.Directory?.FullName);
-                    if (!audioFile.Exists)
-                    {
-                        var message = $"Sound {soundName} not found";
-                        if (command.HasResponded)
-                            await command.ModifyOriginalResponseAsync(m => m.Content = message);
-                        else
-                            await command.RespondAsync(message);
-                        throw new FileNotFoundException(message, audioFile.FullName);
-                    }
+                    var message = $"Sound {soundName} not found";
+                    if (command.HasResponded)
+                        await command.ModifyOriginalResponseAsync(m => m.Content = message);
                     else
-                    {
-                        var targetChannel = command.Data.Options.Single(x => x.Name == "channel").Value as SocketChannel;
-                        if (targetChannel is SocketVoiceChannel voiceChannel)
-                        {
-                            _logger.LogDebug("Connecting to channel {vc}", voiceChannel.Name);
-                            var audioClient = await _audioClientManager.JoinChannelAsync(voiceChannel);
-                            try
-                            {
-                                _logger.LogDebug("Getting audio player for channel {voiceChannelName}", voiceChannel.Name);
-                                var player = _audioClientManager.GetAudioPlayer(audioClient);
-                                _logger.LogDebug("Starting ffmpeg process");
-                                using (var ffmpegProcess = Process.Start(new ProcessStartInfo
-                                {
-                                    FileName = "ffmpeg",
-                                    Arguments = $"-hide_banner -loglevel panic -i \"{audioFile.FullName}\" -ac 2 -f s16le -ar 48000 pipe:1",
-                                    UseShellExecute = false,
-                                    RedirectStandardOutput = true
-                                }))
-                                {
-                                    _logger.LogDebug("Playing stream");
-                                    await player.PlayAsync(ffmpegProcess.StandardOutput.BaseStream);
-                                    await Task.Delay(10); //Deleay channel leaving after sound to not instantly hear the leaving sound
-                                }
-                            }
-                            finally
-                            {
-                                _logger.LogDebug("Leaving channel");
-                                await _audioClientManager.LeaveChannelAsync(voiceChannel);
-                                await command.ModifyOriginalResponseAsync(m => m.Content = $"Finished playing {soundName}");
-                            }
-                        }
-                    }
+                        await command.RespondAsync(message);
+                    throw new FileNotFoundException(message, audioFile.FullName);
                 }
-                catch (Exception ex)
+                else
                 {
-                    _logger.LogError(ex, "Sound command threw an exception");
+                    var targetChannel = command.GetRequiredOptionValue<IVoiceChannel>("channel");
+                    var queueCount = _audioQueueManager.GetQueueCount(command.GuildId.Value);
+
+                    var queueEntry = new AudioQueueEntry(
+                        targetChannel,
+                        new Lazy<AudioStreamElements>(() => CreateAudioStream(audioFile)),
+                        () => SendNowPlaying(soundName, command, queueCount == 0),
+                        () => _logger.LogDebug("Finished playing"),
+                        new Dictionary<string, object?>() { { "CommandCallID", command.Id } }
+                        );
+                    var queuePosition = _audioQueueManager.Add(command.GuildId.Value, queueEntry);
+                    if (queueCount > 0)
+                        await command.ModifyOriginalResponseAsync(m => { m.Content = $"Added to queue on {queuePosition - 1} position"; });                    
                 }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Sound command threw an exception");
+            }
+         
+        }
+
+        private AudioStreamElements CreateAudioStream(FileInfo audioFile)
+        {
+            var ffmpegProcess = Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $"-hide_banner -loglevel panic -i \"{audioFile.FullName}\" -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true
             });
-            return Task.CompletedTask;
+            return new AudioStreamElements(ffmpegProcess.StandardOutput.BaseStream, new[] { ffmpegProcess });
+        }
+
+        private void SendNowPlaying(string soundName, SocketSlashCommand command, bool isResponse)
+        {
+            var message = $"Playing sound {soundName}";
+            if (isResponse)
+                command.ModifyOriginalResponseAsync(m => { m.Content = message; });
+            else
+                command.Channel.SendMessageAsync(message);
         }
     }
 }

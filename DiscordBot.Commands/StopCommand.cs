86219d45d6746fc72babf0e55dad775c115b152a
seker212 <seker212@gmail.com>
2023-07-22 19:30:52 +0200

    Added stop command

---
+++ DiscordBot.Commands/StopCommand.cs
@@ -0,0 +1,40 @@
+﻿using Discord.WebSocket;
+using DiscordBot.Commands.Core;
+using DiscordBot.Commands.Core.CommandAttributes;
+using DiscordBot.Core.Voice;
+using Microsoft.Extensions.Logging;
+
+namespace DiscordBot.Commands
+{
+    [Name("stop")]
+    [Description("Stops playing and clears the queue")]
+    public class StopCommand : Command
+    {
+        private readonly ILogger<StopCommand> _logger;
+        private readonly IAudioQueueManager _audioQueueManager;
+        private readonly IAudioClientManager _audioClientManager;
+
+        public StopCommand(ILogger<StopCommand> logger, IAudioQueueManager audioQueueManager, IAudioClientManager audioClientManager)
+        {
+            _logger = logger;
+            _audioQueueManager = audioQueueManager;
+            _audioClientManager = audioClientManager;
+        }
+
+        public override async Task ExecuteAsync(SocketSlashCommand command)
+        {
+            if (!command.GuildId.HasValue)
+            {
+                _logger.LogWarning("Command has been used on direct message chat");
+                throw new ArgumentNullException(nameof(command.GuildId), "Command has been used on direct message chat");
+            }
+            if (_audioClientManager.HasActiveAudioPlayer(command.GuildId.Value))
+            {
+                await command.RespondAsync("Stopping player and cleaning queue.");
+                await _audioQueueManager.Stop(command.GuildId.Value);
+            }
+            else
+                await command.RespondAsync("Guild has no active audio player.");
+        }
+    }
+}
